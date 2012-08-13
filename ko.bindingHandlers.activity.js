475b48f401b31c1b7bcd74feb1f4349416fd903c
John Papa <john@johnpapa.net>
2012-08-13 14:34:23 -0400

    Added initial files.

---
+++ ko.bindingHandlers.activity.js
@@ -0,0 +1,17 @@
+﻿// By: Hans Fjällemark and John Papa
+// https://github.com/CodeSeven/KoLite
+
+;(function ($, ko) {
+    ko.bindingHandlers.activity = {
+        init: function (element) {
+            ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+                $(element).activityEx(false);
+            });
+        },
+
+        update: function (element, valueAccessor) {
+            var activity = valueAccessor()();
+            typeof activity !== 'boolean' || $(element).activityEx(activity);
+        }
+    };
+})(jQuery, ko);
---
+++ ko.bindingHandlers.command.js
@@ -0,0 +1,28 @@
+﻿// By: Hans Fjällemark and John Papa
+// https://github.com/CodeSeven/KoLite
+
+;(function ($, ko) {
+    ko.bindingHandlers.command = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+            var value = valueAccessor();
+            var commands = value.execute ? { click: value } : value;
+            var events = {};
+            for (var command in commands) {
+                events[command] = commands[command].execute;
+            }
+
+            ko.bindingHandlers.event.init(element, ko.utils.wrapAccessor(events), allBindingsAccessor, viewModel);
+        },
+
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+            var commands = valueAccessor();
+            var canExecute = commands.canExecute || (commands.click ? commands.click.canExecute : null);
+            if (!canExecute) {
+                return;
+            }
+
+            //ko.bindingHandlers.enable.update(element, ko.utils.wrapAccessor(canExecute()), allBindingsAccessor, viewModel);
+            ko.bindingHandlers.enable.update(element, canExecute, allBindingsAccessor, viewModel);
+        }
+    };
+})(jQuery, ko);
